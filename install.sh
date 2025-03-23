b3a2a1d0d094168a9e56f3dfaa893681c5dab5ed
M47V <m47vini@gmail.com>
2025-03-24 02:19:02 +0330

    feat: #3 install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+TARGET="$HOME/.local/bin"  # Installation path
+APP_NAME="seqgen"
+
+# Ensure that path exists
+mkdir -p "$TARGET"
+
+# Copy the executable to the target path
+cp seqgen.sh "${TARGET}/${APP_NAME}"
+
+# Make it executable
+chmod +x "${TARGET}/${APP_NAME}"
+
+# Check that the target path is in PATH
+if [[ ":$PATH:" != *":$TARGET:"* ]]; then
+    echo "Please add `$TARGET` to the PATH and reopen the terminal !"
+fi
+
+echo "Installation complete."
